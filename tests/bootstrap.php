5f8e372b2a70a806e87323db9bde5b48f21b92ce
Thomas Ploch <t.ploch@reizwerk.com>
2012-12-15 13:11:04 +0100

    Fixed broken test bootstrap after namespace changes.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,49 +2,50 @@
 // Include the composer autoloader
 $loader = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-// Register services with the GuzzleTestCase
+// Register default mock response directory
 Guzzle\Tests\GuzzleTestCase::setMockBasePath(__DIR__ . DIRECTORY_SEPARATOR . 'mock');
 
+// Register services with the GuzzleTestCase
 Guzzle\Tests\GuzzleTestCase::setServiceBuilder(\Guzzle\Service\Builder\ServiceBuilder::factory(array(
     'test.dawanda.invalid.lang' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array(
             'language' => 'foooo',
             'api_key'  => $_SERVER['API_KEY']
         )
     ),
     'test.dawanda.invalid.version' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array(
             'version' => '30',
             'api_key'  => $_SERVER['API_KEY']
         )
     ),
     'test.dawanda.no.key' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array()
     ),
     'test.dawanda.no.desc' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array(
             'service' => 'nonexistant.php'
         )
     ),
     'test.dawanda.de' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array(
             'language' => 'de',
             'api_key'  => $_SERVER['API_KEY']
         )
     ),
     'test.dawanda.en' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array(
             'api_key'  => $_SERVER['API_KEY']
         )
     ),
     'test.dawanda.fr' => array(
-        'class' => 'Guzzle\\Dawanda\\DaWandaClient',
+        'class' => 'Dawanda\\DaWandaClient',
         'params' => array(
             'language' => 'fr',
             'api_key'  => $_SERVER['API_KEY']
